44fc66b19ae8b306b4a42dbdea85d4a15e302784
Arthur C Edwards <Arthur.Edwards321@gmail.com>
2019-12-26 01:47:04 -0500

    set up off nav

--- main/assets/js/scripts.js
+++ main/assets/js/scripts.js
@@ -268,3 +268,101 @@ Math.easeInOutQuad = function (t, b, c, d) {
   };
   window.addEventListener('mousedown', detectClick);
 }());
+// File#: _1_header
+// Usage: codyhouse.co/license
+(function() {
+    var mainHeader = document.getElementsByClassName('js-header')[0];
+    if( mainHeader ) {
+      var trigger = mainHeader.getElementsByClassName('js-header__trigger')[0],
+        nav = mainHeader.getElementsByClassName('js-header__nav')[0];
+  
+      // we'll use these to store the node that needs to receive focus when the mobile menu is closed 
+      var focusMenu = false;
+  
+      //detect click on nav trigger
+      trigger.addEventListener("click", function(event) {
+        event.preventDefault();
+        var ariaExpanded = !Util.hasClass(nav, 'header__nav--is-visible');
+        //show nav and update button aria value
+        Util.toggleClass(nav, 'header__nav--is-visible', ariaExpanded);
+        trigger.setAttribute('aria-expanded', ariaExpanded);
+        if(ariaExpanded) { //opening menu -> move focus to first element inside nav
+          nav.querySelectorAll('[href], input:not([disabled]), button:not([disabled])')[0].focus();
+        } else if(focusMenu) {
+          focusMenu.focus();
+          focusMenu = false;
+        }
+      });
+      // listen for key events
+      window.addEventListener('keyup', function(event){
+        // listen for esc key
+        if( (event.keyCode && event.keyCode == 27) || (event.key && event.key.toLowerCase() == 'escape' )) {
+          // close navigation on mobile if open
+          if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger)) {
+            focusMenu = trigger; // move focus to menu trigger when menu is close
+            trigger.click();
+          }
+        }
+        // listen for tab key
+        if( (event.keyCode && event.keyCode == 9) || (event.key && event.key.toLowerCase() == 'tab' )) {
+          // close navigation on mobile if open when nav loses focus
+          if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger) && !document.activeElement.closest('.js-header')) trigger.click();
+        }
+      });
+    }
+  
+    function isVisible(element) {
+      return (element.offsetWidth || element.offsetHeight || element.getClientRects().length);
+    };
+  }());
+// File#: _2_off-canvas-navigation
+// Usage: codyhouse.co/license
+(function() {
+    var OffCanvasNav = function(element) {
+      this.element = element;
+      this.panel = this.element.getElementsByClassName('js-off-canvas__panel')[0];
+      this.trigger = document.querySelectorAll('[aria-controls="'+this.panel.getAttribute('id')+'"]')[0];
+      this.svgAnim = this.trigger.getElementsByTagName('circle');
+      initOffCanvasNav(this);
+    };
+  
+    function initOffCanvasNav(canvas) {
+      if(transitionSupported) {
+        // do not allow click on menu icon while the navigation is animating
+        canvas.trigger.addEventListener('click', function(event){
+          canvas.trigger.style.setProperty('pointer-events', 'none');
+        });
+        canvas.panel.addEventListener('openPanel', function(event){
+          canvas.trigger.style.setProperty('pointer-events', 'none');
+        });
+        canvas.panel.addEventListener('transitionend', function(event){
+          if(event.propertyName == 'visibility') {
+            canvas.trigger.style.setProperty('pointer-events', '');
+          }
+        });
+      }
+  
+      if(canvas.svgAnim.length > 0) { // create the circle fill-in effect
+        var circumference = (2*Math.PI*canvas.svgAnim[0].getAttribute('r')).toFixed(2);
+        canvas.svgAnim[0].setAttribute('stroke-dashoffset', circumference);
+        canvas.svgAnim[0].setAttribute('stroke-dasharray', circumference);
+        Util.addClass(canvas.trigger, 'offnav-control--ready-to-animate');
+      }
+      
+      canvas.panel.addEventListener('closePanel', function(event){
+        // if the navigation is closed using keyboard or a11y close btn -> change trigger icon appearance (from arrow to menu icon) 
+        if(event.detail == 'key' || event.detail == 'close-btn') {
+          canvas.trigger.click();
+        }
+      });
+    };
+  
+    // init OffCanvasNav objects
+    var offCanvasNav = document.getElementsByClassName('js-off-canvas--nav'),
+      transitionSupported = Util.cssSupports('transition');
+    if( offCanvasNav.length > 0 ) {
+      for( var i = 0; i < offCanvasNav.length; i++) {
+        (function(i){new OffCanvasNav(offCanvasNav[i]);})(i);
+      }
+    }
+  }());
